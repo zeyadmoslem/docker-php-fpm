a88c801cf311b715b09c25a084b22759935c19e2
zeyad.moslem <zeyad.moslem@cmf.de>
2022-05-07 23:15:25 +0200

    first

---
+++ README.md
@@ -0,0 +1,12 @@
+# Docker PHP8.1-FPM 🐳
+
+> A complete development environment for building Web projects using Docker
+
+Included Features:
+
+- Nginx
+- MySQL
+- PHP8.1-FPM
+- PHPMyAdmin
+- Nodejs v12, v14, v16 (including npm, yarn)
+- Composer
---
+++ docker/php/start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+##
+# Ensure /.composer is writable
+#
+chmod -R ugo+rw /.composer
+
+##
+# Run a command or start supervisord
+#
+if [ $# -gt 0 ];then
+    # If we passed a command, run it
+    exec "$@"
+else
+    # Otherwise start supervisord
+    /usr/bin/supervisord
+fi
